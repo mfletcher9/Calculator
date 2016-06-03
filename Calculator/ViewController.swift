94e86637cc6824faa6b7076b389cd4e5738f6692
Matt Fletcher <mfletcher@ibji.com>
2016-06-03 09:39:59 -0500

    My first commit!

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -2,24 +2,38 @@
 //  ViewController.swift
 //  Calculator
 //
-//  Created by Matt Fletcher on 6/3/16.
+//  Created by Matt Fletcher on 6/2/16.
 //  Copyright © 2016 Matt Fletcher. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func performOperation(sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            if mathematicalSymbol == "π" {
+                display.text = String(M_PI)
+            }
+        }
+        
     }
-
-
+    
 }
-
